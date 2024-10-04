c61a3a4978a06d0737f96134ae05799dd99636a1
Pratik <143311891+PratikDakhole10@users.noreply.github.com>
2024-10-04 21:20:58 +0530

    insert data in category table

--- Economic Website/Controllers/Data/ApplicationDbContext.cs
+++ Economic Website/Controllers/Data/ApplicationDbContext.cs
@@ -10,5 +10,14 @@ namespace Economic_Website.Controllers.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                  new Category { Id = 1,Name = "Action", DisplayOrders = 1 },
+                  new Category { Id = 2, Name = "Scifi", DisplayOrders = 2 },
+                  new Category { Id = 3, Name = "History", DisplayOrders = 3 }
+                );
+        }
     }
 }
--- Economic Website/Migrations/20241004154814_SeedCategory.Designer.cs
+++ Economic Website/Migrations/20241004154814_SeedCategory.Designer.cs
@@ -3,6 +3,7 @@ using Economic_Website.Controllers.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Economic_Website.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241004154814_SeedCategory")]
+    partial class SeedCategory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
