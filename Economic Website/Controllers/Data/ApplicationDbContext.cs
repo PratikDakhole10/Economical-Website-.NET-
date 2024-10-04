471e1bc5c899924897ca652c360fb83b3730fa6a
Pratik <143311891+PratikDakhole10@users.noreply.github.com>
2024-10-04 11:56:29 +0530

    Models and Connections

---
+++ Economic Website/Controllers/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Economic_Website.Controllers.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+                
+        }
+    }
+}
