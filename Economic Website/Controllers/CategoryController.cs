a7ccd7699fcc3f3bab561c41a3fb66bf9331e76d
Pratik <143311891+PratikDakhole10@users.noreply.github.com>
2024-10-05 00:15:49 +0530

    retrieve data from table

--- Economic Website/Controllers/CategoryController.cs
+++ Economic Website/Controllers/CategoryController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Economic_Website.Controllers.Data;
+using Economic_Website.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Economic_Website.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
